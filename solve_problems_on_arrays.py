49ca64860bcda48b2897ddea788ba4464e35ed17
vimalsingh135 <be23b042@smail.iitm.ac.in>
2026-07-13 23:51:38 +0530

    updating files

--- solve_problems_on_arrays.py
+++ solve_problems_on_arrays.py
@@ -44,3 +44,46 @@ def remove_duplicates(arr):
 
 arr = [1,2,2,3,4,4,5]
 print("array after removing duplicates:", remove_duplicates(arr))
+
+## maximum consecutive ones in an array 
+def max_consecutive_ones():
+    arr = [1, 1, 0, 1, 1, 1,0,1,1,1,1]
+    max_count = 0
+    count = 0
+    for i in range(len(arr)):
+        if arr[i] == 1:
+            count += 1
+            max_count = max(max_count, count)
+        else:
+            count = 0
+    return max_count
+print("maximum consecutive ones:", max_consecutive_ones())
+
+
+## find the number that appears once in an array and other number appears twice
+def find_single_number(arr):
+    result = 0
+    for num in arr:
+        result ^= num
+    return result
+
+arr = [1, 2, 3, 2, 1]
+print("the number that appears once:", find_single_number(arr))
+
+## longest subarray with given sum k(positive integers)
+def longest_subarray_with_sum_k(arr, k):
+    max_length = 0
+    current_sum = 0
+    start = 0
+
+    for end in range(len(arr)):
+        current_sum += arr[end]
+
+        while current_sum > k and start <= end:
+            current_sum -= arr[start]
+            start += 1
+
+        if current_sum == k:
+            max_length = max(max_length, end - start + 1)
+
+    return max_length
