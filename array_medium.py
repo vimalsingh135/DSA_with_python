3b9ec486144741390cc486958af135475ecd9120
vimalsingh135 <be23b042@smail.iitm.ac.in>
2026-07-16 17:34:32 +0530

    updating files

--- array_medium.py
+++ array_medium.py
@@ -86,3 +86,78 @@ def max_subarray_sum_kadane(arr):
 
     return max_sum
 print("maximum subarray sum:", max_subarray_sum_kadane([-2,1,-3,4,-1,2,1,-5,4]))
+
+## stock buy and sell problem
+def max_profit(prices):
+    min_profit=float('inf')
+    max_profit=0
+    for price in prices:
+        if price<min_profit:
+            min_profit=price
+        elif price-min_profit>max_profit:
+            max_profit=price-min_profit
+    return max_profit
+print("maximum profit:", max_profit([7,1,5,3,6,4]))
+
+## rearranging array elements by sign
+def rearrangment(arr):
+    positives=[]
+    negatives=[]
+    n=len(arr)
+    for i in range(n):
+        if arr[i]>0:
+            positives.append(arr[i])
+        elif arr[i]<0:
+            negatives.append(arr[i])
+    result=[]
+    i,j=0,0
+    while i<len(positives) and j<len(negatives):
+        result.append(positives[i])
+        result.append(negatives[j])
+        i+=1
+        j+=1
+
+    return result
+print("the rearranging array elements are:",rearrangment(arr=[1,2,-4,-5]) )
+
+## next permutation
+def nextPermutation(arr):
+    n = len(arr)
+    
+    # Step 1: find pivot (first decreasing point from right)
+    i = n - 2
+    while i >= 0 and arr[i] >= arr[i+1]:
+        i -= 1
+    
+    if i >= 0:  # pivot found (not fully descending)
+        # Step 2: find smallest element > arr[i] from the right side
+        j = n - 1
+        while arr[j] <= arr[i]:
+            j -= 1
+        arr[i], arr[j] = arr[j], arr[i]  # swap
+    
+    # Step 3: reverse the suffix after i (works even if i == -1, reverses whole array)
+    arr[i+1:] = reversed(arr[i+1:])
+    
+    return arr
+print("the next permutation array:", nextPermutation(arr=[1,2,3]) )
+
+## longest concecutive sequence array
+def longestConsecutive(nums):
+    num_set = set(nums)
+    longest = 0
+    
+    for num in num_set:
+        if num - 1 not in num_set:   # only start counting if num is a sequence start
+            current = num
+            length = 1
+            
+            while current + 1 in num_set:
+                current += 1
+                length += 1
+            
+            longest = max(longest, length)
+    
+    return longest
+
+print("the lonest concecutive sequence is:",longestConsecutive(nums=[1,2,100,101,200,3,4,5]) )
