425e87a5bc2559b951318951ababd88b2b60f882
vimalsingh135 <be23b042@smail.iitm.ac.in>
2026-07-28 23:22:43 +0530

    updating files

--- array_IV.py
+++ array_IV.py
@@ -72,3 +72,69 @@ def myPow(x, n):
 print(myPow(2.0, 10))
 print(myPow(2.0, -2))   
 
+## Find the Majority Element that occurs more than N/2 times
+## brute force approach
+def majority_element(nums):
+    n=len(nums)
+    for i in range(n):
+        count=0
+        for j in range(n):
+            if nums[j]==nums[i]:
+                count+=1
+        if count>n//2:
+            return nums[i]
+    return -1
+print ("the that occurs more than N/2 times is:", majority_element(nums=[7, 0, 0, 1, 7, 7, 2, 7, 7]))
+
+## Better: Hashmap (O(n) time, O(n) space):
+def majorityElement_hashmap(nums):
+    count = {}
+    n = len(nums)
+    for num in nums:
+        count[num] = count.get(num, 0) + 1
+        if count[num] > n // 2:
+            return num
+    return -1
+print ("the that occurs more than N/2 times is:", majority_element(nums=[7, 0, 0, 1, 7, 7, 2, 7, 7]))
+
+## Optimal: Boyer-Moore Voting Algorithm (O(n) time, O(1) space)
+def majorityElement(nums):
+    count = 0
+    candidate = None
+
+    for num in nums:
+        if count == 0:
+            candidate = num
+        count += 1 if num == candidate else -1
+
+    return candidate
+
+print(majorityElement([7,0,0,1,7,7,2,7,7]))  
+
+## Majority Elements(&gt;N/3 times) | Find the elements that appears more than N/3 times in the array
+def majority_E(nums):
+    n=len(nums)
+    for i in range (n):
+        count=0
+        for j in range(n):
+            if nums[j]==nums[i]:
+                count+=1
+        if count>n//3:
+            return nums[i]
+print ("the majority element is", majority_E(nums=[1, 2, 1, 1, 3, 2] ))
+
+# hashmap
+def maj_element(nums):
+    count={}
+    n=len(nums)
+    for i in range (n):
+        count[nums]= count.get(nums,0) + 1
+        if count>n//3:
+            return nums
+print ("the majority element is", majority_E(nums=[1, 2, 1, 1, 3, 2] ))
+
+## Grid Unique Paths : DP on Grids (DP8)
+def grid_unique_paths(arr,n,m):
+    x=len(arr)
+    for n in range(x):
+        for 
