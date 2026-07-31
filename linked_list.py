84374b45bad4ee0013bc7d1b04d620b716787408
vimalsingh135 <be23b042@smail.iitm.ac.in>
2026-07-31 18:45:17 +0530

    updating the files

--- linked_list.py
+++ linked_list.py
@@ -97,3 +97,116 @@ print("Middle of odd list:", findMiddle(head1).data)  # 3
 arr2 = [1, 2, 3, 4, 5, 6]
 head2 = convertArr2LL(arr2)
 print("Middle of even list:", findMiddle(head2).data)  # 4
+
+## Merge two sorted Linked Lists
+class Node:
+    def __init__(self, data1, next1=None):
+        self.data = data1
+        self.next = next1
+
+
+def mergeTwoLists(list1, list2):
+    dummyNode = Node(-1)   # placeholder to simplify edge cases
+    temp = dummyNode
+
+    while list1 is not None and list2 is not None:
+        if list1.data <= list2.data:
+            temp.next = list1
+            list1 = list1.next
+        else:
+            temp.next = list2
+            list2 = list2.next
+        temp = temp.next
+
+    # attach whichever list still has leftover nodes
+    if list1 is not None:
+        temp.next = list1
+    else:
+        temp.next = list2
+
+    return dummyNode.next  # skip the dummy, real head starts here
+
+
+# ---- helper functions ----
+def convertArr2LL(arr):
+    if not arr:
+        return None
+    head = Node(arr[0])
+    curr = head
+    for i in range(1, len(arr)):
+        curr.next = Node(arr[i])
+        curr = curr.next
+    return head
+
+
+##Remove N-th node from the end of a Linked List
+def printLL(head):
+    curr = head
+    while curr is not None:
+        print(curr.data, end=" -> ")
+        curr = curr.next
+    print("None")
+
+
+# ---- driver code ----
+list1 = convertArr2LL([2, 4, 8, 10])
+list2 = convertArr2LL([1, 3, 3, 6, 11, 14])
+
+merged = mergeTwoLists(list1, list2)
+print("Merged List: ", end="")
+printLL(merged)
+
+##
+class Node:
+    def __init__(self, data1, next1=None):
+        self.data = data1
+        self.next = next1
+
+
+def removeNthFromEnd(head, n):
+    dummyNode = Node(-1)
+    dummyNode.next = head
+    fast = dummyNode
+    slow = dummyNode
+
+    # move fast n steps ahead
+    for _ in range(n):
+        fast = fast.next
+
+    # move both until fast hits the last node
+    while fast.next is not None:
+        fast = fast.next
+        slow = slow.next
+
+    # slow is now right before the node to delete
+    slow.next = slow.next.next
+
+    return dummyNode.next
+
+
+# ---- helper functions ----
+def convertArr2LL(arr):
+    head = Node(arr[0])
+    curr = head
+    for i in range(1, len(arr)):
+        curr.next = Node(arr[i])
+        curr = curr.next
+    return head
+
+
+def printLL(head):
+    curr = head
+    while curr is not None:
+        print(curr.data, end="->" if curr.next else "")
+        curr = curr.next
+    print()
+
+
+# ---- driver code ----
+head = convertArr2LL([5, 1, 2])
+result = removeNthFromEnd(head, 2)
+printLL(result)   # 5->2
+
+head2 = convertArr2LL([1, 2, 3, 4, 5])
+result2 = removeNthFromEnd(head2, 3)
+printLL(result2)  # 1->2->4->5
