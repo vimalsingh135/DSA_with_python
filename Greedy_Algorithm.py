ac9b8fe37639cd81abfe4b60fccd0e127bc9be24
vimalsingh135 <be23b042@smail.iitm.ac.in>
2026-08-05 23:55:43 +0530

    updating file

--- Greedy_Algorithm.py
+++ Greedy_Algorithm.py
@@ -16,3 +16,25 @@ def n_meetings(start,end,n):
             last_end_time = meeting[1]
     
     return result
+print ("the maximum meetings that can be held are: ", n_meetings([1, 3, 0, 5, 8, 5], [2, 4, 6, 7, 9, 9], 6))
+
+## Minimum number of platforms required for a railway
+def min_platforms(arr, dep, n):
+    arr.sort()
+    dep.sort()
+    
+    platforms = 1
+    max_platforms = 1
+    i = 1
+    j = 0
+    
+    while i < n and j < n:
+        if arr[i] < dep[j]:
+            platforms += 1
+            max_platforms = max(max_platforms, platforms)
+            i += 1
+        else:
+            platforms -= 1
+            j += 1
+    
+    return max_platforms
