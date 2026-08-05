3bd2b8cb50f1acc3d7e2c908900b21d5edbc5a4b
vimalsingh135 <be23b042@smail.iitm.ac.in>
2026-08-05 23:09:15 +0530

    updating file

--- Greedy_Algorithm.py
+++ Greedy_Algorithm.py
@@ -1,1 +1,18 @@
-## N number
+## N meetings in one room
+def n_meetings(start,end,n):
+    meetings = []
+    for i in range(n):
+        meetings.append((start[i], end[i], i+1))
+    
+    # Sort meetings based on their end time
+    meetings.sort(key=lambda x: x[1])
+    
+    result = []
+    last_end_time = 0
+    
+    for meeting in meetings:
+        if meeting[0] > last_end_time:
+            result.append(meeting[2])  # Append meeting number
+            last_end_time = meeting[1]
+    
+    return result
