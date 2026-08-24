3431f431f36c3e9c9bb05b5a549c8887a883dc62
vimalsingh135 <be23b042@smail.iitm.ac.in>
2026-08-25 00:19:49 +0530

    updating new files

--- heaps.py
+++ heaps.py
@@ -13,4 +13,44 @@ class solution:
             self._shiftUp(index)
         else:
             self._sdhiftDown(index)
-    
+    def extractMax(self):
+        if len(self.heap)==0:
+            return None
+        maxValue=self.heap[0]
+        self.heap[0]=self.heap[-1]
+        self.heap.pop()
+        self._shiftDown(0)
+        return maxValue
+    def isempty(self):
+        return len(self.heap)==0
+    def getmax(self):
+        return self.heap[0] if self.heap else None
+    def heapsize(self):
+        return len(self.heap)
+
+    def _shiftUp(self, index):
+        while index>0:
+            parent=(index-1)//2
+            if self.heap[index]>self.heap[parent]:
+                self.heap[index],self.heap[parent]=self.heap[parent],self.heap[index]
+                index=parent
+            else:
+                break
+    def _shiftDown(self, index):
+        size=len(self.heap)
+        while index<size:
+            left=2*index+1
+            right=2*index+2
+            largest=index
+            if left<size and self.heap[left]>self.heap[largest]:
+                largest=left
+            if right<size and self.heap[right]>self.heap[largest]:
+                largest=right
+            if largest!=index:
+                self.heap[index],self.heap[largest]=self.heap[largest],self.heap[index]
+                index=largest
+            else:
+                break
+
+print("Heap Implementation")
+heap=solution()
