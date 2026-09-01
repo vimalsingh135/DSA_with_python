22abf9f078d150822c8dbcb4054cd229ae67250f
vimalsingh135 <be23b042@smail.iitm.ac.in>
2026-09-01 14:44:56 +0530

    adding new files and working

--- Stack_&_Queue.py
+++ Stack_&_Queue.py
@@ -51,5 +51,68 @@ if __name__ == "__main__":
 
 # implementation of queue in data stucture in python
 class ArrayQueue:
-    def queue(self,size=1000):
+    def __init__(self):
+        self.size=[0]*10
+        self.start=-1
+        self.end=-1
+        self.currsize=0
+        self.maxsize=10
+        
+
+    def enqueue(self,x):
+
+        if self.currsize==self.maxsize:
+            print("the queue is full/exiting...")
+            exit(1)
+        if self.end==-1:
+            self.start=0
+            self.end=0
+
+        else:
+            self.end=(self.end+1)%self.maxsize
+        self.size[self.end]=x
+        self.currsize+=1
+
+    def dequeue(self):
+        if self.currsize==0:
+            print("the queue is empty/existing...")
+            exit(1)
+        dequeued_element=self.size[self.start]
+
+        if self.start== self.end:
+            self.start = -1
+            self.end = -1
+        else:
+            self.start=(self.start+1)%self.maxsize
+        self.currsize-=1
+        return dequeued_element
+
+    def peek(self):
+        if self.currsize==-1:
+            print("the queue is empty/existing...")
+            exit(1)
+        return (self.size[self.start])
+
+    def _isempty(self):
+        return self.currsize==0
+
+
+if __name__ == "__main__":
+    queue = ArrayQueue()
+    commands = ["ArrayQueue", "enqueue", "dequeue", "peek", "dequeue", "isEmpty"]
+    inputs = [[], [5], [10], [], [], []]
+
+    for i in range(len(commands)):
+        if commands[i] == "enqueue":
+            queue.enqueue(inputs[i][0])
+            print("null", end=" ")
+        elif commands[i] == "dequeue":
+            print(queue.dequeue(), end=" ")
+        elif commands[i] == "peek":
+            print(queue.peek(), end=" ")
+        elif commands[i] == "isEmpty":
+            print("true" if queue._isempty() else "false", end=" ")
+        elif commands[i] == "ArrayQueue":
+            print("null", end=" ")
+
         
