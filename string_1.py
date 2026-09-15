72e3a29c430ee72b0757e9fa9849c55c87ffec53
vimalsingh135 <be23b042@smail.iitm.ac.in>
2026-09-16 02:53:30 +0530

    finishing the part one of string

--- string_1.py
+++ string_1.py
@@ -78,3 +78,80 @@ print ("the longest palindromic substring is :", longest_palindrome("babad"))
 print ("the longest palindromic substring is :", longest_palindrome("cbbdkkanabidabcasasasasa"))
 
 ## Roman Numerals to Integer
+def roman_to_integer(s):
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    prev_value = 0
+    
+    for char in reversed(s):
+        value = roman_dict[char]
+        
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        
+        prev_value = value
+    
+    return total
+
+print("The integer value of the Roman numeral is:", roman_to_integer("III"))
+print("The integer value of the Roman numeral is:", roman_to_integer("IV"))
+print("The integer value of the Roman numeral is:", roman_to_integer("IX"))
+print("The integer value of the Roman numeral is:", roman_to_integer("LVIII"))
+print("The integer value of the Roman numeral is:", roman_to_integer("MCMXC"))
+
+
+## Longest Common Prefix
+class Solution:
+    # Returns the longest common prefix from a list of strings
+    def longestCommonPrefix(self, strs):
+        # Handle empty list case
+        if not strs:
+            return ""
+        
+        # Sort the list lexicographically
+        strs.sort()
+        
+        # First string in sorted list
+        first = strs[0]
+        
+        # Last string in sorted list
+        last = strs[-1]
+        
+        # Store the common prefix characters
+        ans = []
+        
+        # Compare characters of first and last string
+        for i in range(min(len(first), len(last))):
+            # Stop if characters differ
+            if first[i] != last[i]:
+                return ''.join(ans)
+            # Add matching character to result
+            ans.append(first[i])
+        
+        # Return the longest common prefix
+        return ''.join(ans)
+
+# Run the function with sample input
+if __name__ == "__main__":
+    # Create an instance of Solution
+    solution = Solution()
+    
+    # Input list of strings
+    input_strs = ["interview", "internet", "internal", "interval"]
+    
+    # Call the method to find prefix
+    result = solution.longestCommonPrefix(input_strs)
+    
+    # Print the result
+    print("Longest Common Prefix:", result) 
