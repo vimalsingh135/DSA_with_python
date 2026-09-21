354f5a996462f70747597134b660d8133d314ed8
vimalsingh135 <be23b042@smail.iitm.ac.in>
2026-09-21 22:48:46 +0530

    solving house robber problem

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -54,3 +54,18 @@ class Solution:
 print(Solution().frogJump([10, 20, 30, 10], 2))    # 20
 print(Solution().frogJump([10, 20, 10], 1))        # 20
 print(Solution().frogJump([10, 5, 20, 0, 15], 2))  # 15
+
+## HOUSE ROBBER PROBLEM
+class Solution:
+    def rob(self, nums):
+        n=len(nums)
+        dp=[0]*n
+        dp[0]=nums[0]
+        dp[1]=max(nums[0],nums[1])
+        for i in range(2,n):
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i])
+        return dp[n-1]
+
+print(Solution().rob([1,2,3,1]))  # 4
+print(Solution().rob([2,7,9,3,1]))  # 12
+print(Solution().rob([2,1,1,2]))  # 4
